17e714c016100ddfaaf83cb1be44a321f3544252
ritikaguptams <85255050+ritikaguptams@users.noreply.github.com>
2022-02-04 09:01:19 -0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -5,3 +5,4 @@ Write-Host "Your commit to repo was successful"
 #testing tobais's 2022 image
 #testing tobais's 2022 image locally
 #testing to see the error
+#breaking thr runner to get back the ps windows
